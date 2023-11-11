7c6422c74b28a9584aa5ba5dfba482bd213a139e
Vadym <djoh@proxied.host>
2023-11-11 17:25:42 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'];
+    var member = {
+        name: 'John Doe',
+        age: 25,
+        skills: skills
+    };
+    console.log(member.skills[2]);
+}
